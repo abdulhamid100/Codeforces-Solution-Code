6776980198372a6081e9b55c44f51238aeeb270d
Md Abdul Hamid (...) <57268158+abdulhamid100@users.noreply.github.com>
2020-04-22 23:43:06 +0600

    sorting algorithm

---
+++ quiksort.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int partition (int arr[], int low, int high)
+{
+	int pivot = arr[high];
+	int i = (low - 1);
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			i++;
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		int pi = partition(arr, low, high);
+
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+void printArray(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+int main()
+{
+    cout<<"Enter the array size: ";
+    int size;
+    cin>>size;
+    int arr[size],num;
+    cout<<"Enter array element: ";
+    for(int i=0; i<size; i++)
+    {
+    	cin>>num;
+    	arr[i] = num;
+	}
+	quickSort(arr, 0, size - 1);
+	cout << "Sorted array: \n";
+	printArray(arr, size);
+	return 0;
+}
